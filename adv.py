0240d0fcf508b4130e6abf7740630dd02dae5675
Thomas Cacciatore <tcacciatore597@gmail.com>
2019-11-20 16:26:41 -0500

    getting a 200 back from server for movement

--- adv.py
+++ adv.py
@@ -1,6 +1,7 @@
 
 import requests
 import time
+import json
 
 url = "https://lambda-treasure-hunt.herokuapp.com/api/adv/move/"
 
@@ -17,11 +18,11 @@ class Adventure:
         backTrack = []
 
     #setup starting room 0
-        self.roomDict[self.currentRoom] = ["n", "s", "e", "w"]
-        self.roomTitle = "A Bright Room"
-        self.exits = ["n", "s", "e", "w"]
+        self.roomDict[self.currentRoom] = ["s", "n", "e", "w"]
+        self.roomTitle = "A brightly lit room"
+        self.exits = ["s", "n", "e", "w"]
 
-        while self.roomTitle != "Pirate Ry":
+        while self.roomTitle is not "Pirate Ry":
 
     # while len(self.roomDict) < 499:
     
@@ -37,12 +38,14 @@ class Adventure:
                 back = backTrack.pop()
                 path.append(back)
             #call to move player pass in direction (back)
-                r = requests.post(url, data = {"direction": back}, headers =  {"Authorization": "Token 630d593ddd37dfd19902d98ef025638b39131316", "Content-Type": "application/json"})
-                data = r.json()
-                self.currentRoom = data.get("room_id")
-                self.roomTitle = data.get("title")
-                self.exits = data.get("exits")
-                self.cooldown = data.get("cooldown")
+                r = requests.post(url, json = {"direction": back}, headers = {"Authorization": "Token 630d593ddd37dfd19902d98ef025638b39131316", "Content-Type": "application/json"})
+                print(r.status_code)
+                data = json.loads(r.text)
+                self.currentRoom = data["room_id"]
+                self.roomTitle = data["title"]
+                self.exits = data["exits"]
+                self.cooldown = data["cooldown"]
+                print(self.currentRoom)
 
             move = self.roomDict[self.currentRoom].pop(0)
             path.append(move)
@@ -57,16 +60,18 @@ class Adventure:
                 backTrack.append("e")
         
         #travel to next room passing in (move)
-            r = requests.post(url, data = {"direction": move}, headers =  {"Authorization": "Token 630d593ddd37dfd19902d98ef025638b39131316", "Content-Type": "application/json"})
-            data = r.json()
-            self.currentRoom = data.get("room_id")
-            self.roomTitle = data.get("title")
-            self.exits = data.get("exits")
-            self.cooldown = data.get("cooldown")
+        
+            r = requests.post(url, json = {"direction": move}, headers = {"Authorization": "Token 630d593ddd37dfd19902d98ef025638b39131316", "Content-Type": "application/json"})
+            print(r.status_code)
+            data = json.loads(r.text)
+            self.currentRoom = data["room_id"]
+            self.roomTitle = data["title"]
+            self.exits = data["exits"]
+            self.cooldown = data["cooldown"]
+            print(self.currentRoom)
 
         print(path)
         return path
-
 
 adv = Adventure()
 adv.traverse()
